1675af6d6d3ba591d32efe90a357df8c9b5195a6
Qanita Syafika <qanita.syafika@ui.ac.id>
2026-09-20 12:24:47 +0700

    force update settings

--- settings.py
+++ settings.py
@@ -8,5 +8,8 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 USE_X_FORWARDED_HOST = True
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://*.pws.cs.ui.ac.id",
+    "http://localhost",
+    "http://127.0.0.1",
+    "http://qanita-syafika-myportofolio.pws.cs.ui.ac.id",
+    "https://qanita-syafika-myportofolio.pws.cs.ui.ac.id",
 ]
