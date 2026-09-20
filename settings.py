875db48ffef08a3236a125933d1fd5784672c260
Qanita Syafika <qanita.syafika@ui.ac.id>
2026-09-20 12:17:10 +0700

    fix: update csrf trusted origins

--- settings.py
+++ settings.py
@@ -2,13 +2,14 @@ ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
     "qanita-syafika-myportofolio.pws.cs.ui.ac.id",
-    "*.pws.cs.ui.ac.id",
 ]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 USE_X_FORWARDED_HOST = True
 
 CSRF_TRUSTED_ORIGINS = [
+    "http://localhost",
+    "http://127.0.0.1",
+    "http://qanita-syafika-myportofolio.pws.cs.ui.ac.id",
     "https://qanita-syafika-myportofolio.pws.cs.ui.ac.id",
-    "https://*.pws.cs.ui.ac.id",
 ]
